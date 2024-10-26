e13bb4f88d6d41339bbe83c23b5a3676f788b21a
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2024-10-26 16:20:16 +0300

    add rust lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -72,6 +72,21 @@ return {
         rubocop = {
           cmd = { "rubocop", "--lsp" },
         },
+        taplo = {
+          keys = {
+            {
+              "K",
+              function()
+                if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+                  require("crates").show_popup()
+                else
+                  vim.lsp.buf.hover()
+                end
+              end,
+              desc = "Show Crate Documentation",
+            },
+          },
+        },
       },
     },
   },
