edeee91b9d351490ab29ec83b6f279ffe2c001f2
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2025-01-16 21:27:21 +0100

    Add rust analyzer support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,3 +14,8 @@ require("lazy").setup({
 vim.o.linebreak = true
 vim.g.autoformat = false
 vim.opt.relativenumber = false
+-- LSP Server to use for Rust.
+-- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
+-- only for diagnostics. The rest of LSP support will still be
+-- provided by rust-analyzer.
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
