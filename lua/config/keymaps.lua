56c0e5aede4b9d6a4c6b038be7b1c6e759b109dc
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2025-03-09 20:51:37 +0100

    Fix double leader to call proper serach buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,7 +22,9 @@ vim.keymap.set("i", "оо", "<Esc>")
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set('n', '<leader>h', vim.diagnostic.open_float)
+
