0bfc65b5eba2d4f9ded84ee6daa7ad64be69b01f
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2025-03-15 13:44:58 +0100

    Install and use monokai-pro

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,18 @@
+-- return {
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--     },
+--   },
+-- }
+
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+  "loctvl842/monokai-pro.nvim",
+  config = function()
+    require("monokai-pro").setup({
+      filter = "spectrum", -- pro | spectrum | classic | machine | octagon | ristertto
+    })
+    vim.cmd([[colorscheme monokai-pro]])
+  end,
 }
