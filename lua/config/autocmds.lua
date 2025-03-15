061e713b26ba7208a9f1c4b1d108efea7fce7d2e
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2025-03-15 10:35:17 +0100

    Wrap lines

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.opt.wrap = true
 vim.g.blamer_enabled = true
 vim.opt.colorcolumn = "120" -- show vertical line
 vim.opt.cursorline = true
